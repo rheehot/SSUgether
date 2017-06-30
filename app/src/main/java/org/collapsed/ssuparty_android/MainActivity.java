c711b94dfede7a1033de71fb5df047c08f3f339a
Ace Kim <ace.jckim@gmail.com>
2017-06-30 15:19:17 +0900

    Revert "쓸데없는 커밋1"
    This reverts commit dc8a84dfd0e7a0125d2c195791e9c4b2585ebb4d.

--- app/src/main/java/org/collapsed/ssuparty_android/MainActivity.java
+++ app/src/main/java/org/collapsed/ssuparty_android/MainActivity.java
@@ -2,23 +2,12 @@ package org.collapsed.ssuparty_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ArrayList<String> mArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mArray = new ArrayList<>();
-
-        mArray.add("Test");
-
-        Log.d("TAG", mArray.get(0));
     }
 }
