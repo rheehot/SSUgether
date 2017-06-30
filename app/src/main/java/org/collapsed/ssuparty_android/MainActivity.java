147b5a8f24e2ce80571fb1c683effc895998d0e4
Ace Kim <ace.jckim@gmail.com>
2017-06-30 16:11:29 +0900

    [main] 메인 기능 개발 완료

--- app/src/main/java/org/collapsed/ssuparty_android/MainActivity.java
+++ app/src/main/java/org/collapsed/ssuparty_android/MainActivity.java
@@ -2,6 +2,7 @@ package org.collapsed.ssuparty_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(getBaseContext(), "Printf main", Toast.LENGTH_SHORT).show();
     }
 }
