48f6cfe4ca9b74a7021462a8e833d190c9206d6f
Ace Kim <ace.jckim@gmail.com>
2017-07-11 03:15:50 +0900

    [Dev] HttpRequest / Gson을 이용한 Response 파싱 / 파싱된 데이터를 뷰에 보여주는 예제 작성

--- app/src/main/java/org/collapsed/ssuparty_android/Config.java
+++ app/src/main/java/org/collapsed/ssuparty_android/Config.java
@@ -1,7 +1,9 @@
 package org.collapsed.ssuparty_android;
 
-class Config {
+public class Config {
     private static final String TAG = Config.class.getSimpleName();
+
+    public static final String BASE_BANNER_ADDRESS = "http://52.79.95.19:8000/advertise/banner";
 
     public Config() {
 
--- app/src/main/java/org/collapsed/ssuparty_android/ui/home/HomePresenter.java
+++ app/src/main/java/org/collapsed/ssuparty_android/ui/home/HomePresenter.java
@@ -2,14 +2,46 @@ package org.collapsed.ssuparty_android.ui.home;
 
 import android.support.annotation.NonNull;
 
+import org.collapsed.ssuparty_android.model.ExampleAdBanner;
+import org.collapsed.ssuparty_android.network.ExampleAdBannerFetcher;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
 import static com.google.common.base.Preconditions.checkNotNull;
 
-class HomePresenter implements HomeContract.UserActionListener {
+class HomePresenter implements HomeContract.UserActionListener, HomeContract.OnDataFetchedListener {
     private static final String TAG = HomePresenter.class.getSimpleName();
 
     private HomeFragment mView;
 
     public HomePresenter(@NonNull HomeFragment view) {
         this.mView = checkNotNull(view);
+    }
+
+    @Override
+    public void fetchNetworkingExample() {
+        ExampleAdBannerFetcher fetcher = new ExampleAdBannerFetcher(this);
+        fetcher.executeFetch();
+    }
+
+    @Override
+    public void onBannerDataFetched(String response) {
+        parseResponseData(response);
+    }
+
+    @Override
+    public void parseResponseData(String response) {
+        JSONObject object;
+        String banners = null;
+        try {
+            object = new JSONObject(response);
+            banners = object.getString("banners");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        List<ExampleAdBanner> bannerDatas = ExampleAdBanner.fromJson(banners);
+        mView.showBannersDataWithToast(bannerDatas);
     }
 }
